290b6f9c7b416e849bf769b951701b6cfe41f3c8
turtlebeasts <52573759+turtlebeasts@users.noreply.github.com>
2021-08-20 11:41:15 +0530

    update
    added functionality of login screen

---
+++ index.js
@@ -0,0 +1,10 @@
+function loading(){
+    document.getElementById('profile_box').style.backgroundImage='none';
+    document.getElementById('profile_box').style.boxShadow='none';
+    document.getElementById('profile_box').innerHTML='Welcome';
+    document.getElementById('profile_box').style.marginLeft+='720px';
+    document.getElementById('profile_box').style.marginTop+='400px';
+    document.getElementById('profile_box').style.color='white';
+    document.getElementById('input').style.display='none';
+    document.getElementById('user').style.display='none';
+}
